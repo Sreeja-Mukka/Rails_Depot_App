b22ff668aa8b38930780fdae9c3926e484e73b6a
Sreeja <mukkasreeja@gmail.com>
2020-10-19 15:48:38 +0530

    emails!

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,22 @@ module DepotApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.action_mailer.delivery_method = :smtp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    Rails.application.configure do
+      config.action_mailer.delivery_method = :smtp
+      config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "domain.of.sender.net",
+      authentication: "plain",
+      user_name: "dave",
+      password: "secret",
+      enable_starttls_auto: true
+      }
+    end
   end
 end
