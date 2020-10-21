28be14213965e4084734eafd71121cf10b8dfc1a
Sreeja <mukkasreeja@gmail.com>
2020-10-21 17:40:50 +0530

    recive mails till part1

--- config/routes.rb
+++ config/routes.rb
@@ -12,14 +12,16 @@ Rails.application.routes.draw do
   get 'sessions/create'
   get 'sessions/destroy'
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
-
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index' , via: :all
+  end
+  resources :support_requests, only: [ :index, :update ]
   resources :products do
     get :who_bought, on: :member
   end
-    
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
